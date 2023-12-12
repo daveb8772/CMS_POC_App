61a40dd862ed642030408a95b403e433302b4d76
prg-azevedo <prg.azevedo@icloud.com>
2023-12-12 02:10:11 +0000

    Added: + Correct the OCPI root path for 2 controllers Code compiles and runs

--- src/main/java/com/github/daveb8772/ocpi/ocpirestservice/controller/ChargingPointController.java
+++ src/main/java/com/github/daveb8772/ocpi/ocpirestservice/controller/ChargingPointController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,6 +16,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
+@RequestMapping("/ocpi")
 public class ChargingPointController {
 
     @Autowired
