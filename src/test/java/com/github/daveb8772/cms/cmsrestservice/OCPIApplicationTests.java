ac8908ee4c73d5713e6d8bdd6819bfdb5b40afa0
prg-azevedo <prg.azevedo@icloud.com>
2024-01-22 21:06:52 +0000

    +Delete old file

--- src/test/java/com/github/daveb8772/cms/cmsrestservice/OCPIApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.github.daveb8772.cms.cmsrestservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OCPIApplicationTests {
-
-}
