71a5e9b143934eb0e7d73427857e743c8dd97fe2
prg-azevedo <prg.azevedo@icloud.com>
2024-01-28 16:29:03 +0000

    +fix for metrics server

--- scripts/deploy-k8s-resources.sh
+++ scripts/deploy-k8s-resources.sh
@@ -7,6 +7,7 @@ POSTGRES_DEPLOYMENT_YAML_PATH="../k8s/postgres-deployment.yaml"
 POSTGRES_SERVICE_YAML_PATH="../k8s/postgres-service.yaml"
 POSTGRES_PVC_YAML_PATH="../k8s/postgres-pvc.yaml"
 METRICS_SERVER_YAML_PATH="../k8s/metrics-server-deployment.yaml"
+CLUSTER_ROLE_YAML_PATH="../k8s/cluster-role-service-account.yaml"
 
 echo "Deploying PostgreSQL PersistentVolumeClaim..."
 kubectl apply -f "$POSTGRES_PVC_YAML_PATH"
@@ -18,9 +19,10 @@ kubectl apply -f "$POSTGRES_SERVICE_YAML_PATH"
 echo "Deploying Application resources..."
 kubectl apply -f "$DEPLOYMENT_YAML_PATH"
 kubectl apply -f "$SERVICE_YAML_PATH"
-echo "Deploying Metrics sever..."
+echo "Deploying Metrics server..."
 kubectl apply -f "$METRICS_SERVER_YAML_PATH"
-
+echo "Deploying Cluster role..."
+kubectl apply -f "$CLUSTER_ROLE_YAML_PATH"
 
 # Force redeployment by updating a timestamp annotation
 echo "Forcing redeployment of the application..."
